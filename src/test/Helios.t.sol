27dbfb43ced07d01b74e83bc48534643cecad560
steven <steven.nevins.js@gmail.com>
2022-02-26 23:14:41 -0500

    add helios and blacksmith

---
+++ src/test/Helios.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import {DSTest} from "ds-test/test.sol";
+import {HeliosBS} from "./blacksmith/Helios.bs.sol";
+import {ERC20} from "../mocks/ERC20.sol";
+
+contract HeliosTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
